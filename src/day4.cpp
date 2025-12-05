03679e58be87bdd8c5e1c01e0f1c05c57ff82ee6
gvlsq <54555927+gvlsq@users.noreply.github.com>
2025-12-05 09:56:07 -0600

    Day 4 part 2

--- src/day4.cpp
+++ src/day4.cpp
@@ -1,3 +1,4 @@
+#include <memory.h>
 #include <stdlib.h>
 #include <string.h>
 
@@ -38,6 +39,14 @@ static void character_grid_init(Character_Grid *grid, FILE *file) {
     }
 }
 
+static void character_grid_set_char(Character_Grid *grid,
+                                    int x, int y,
+                                    char c) {
+    size_t stride = sizeof(*grid->data)*grid->width;
+
+    *(s8 *)((u8 *)grid->data + y*stride + x) = c;
+}
+
 char character_grid_get_char(Character_Grid *grid,
                              int x, int y,
                              int xoffset = 0, int yoffset = 0) {
@@ -62,26 +71,23 @@ char character_grid_get_char(Character_Grid *grid,
 
 #pragma endregion
 
-u64 day_4_part_1(FILE *file) {
+static u64 count_forkliftable_rolls(Character_Grid *character_grid) {
     u64 result = 0;
 
-    Character_Grid character_grid;
-    character_grid_init(&character_grid, file);
-
-    for (int y = 0; y < character_grid.height; y++) {
-        for (int x = 0; x < character_grid.width; x++) {
-            bool is_roll = character_grid_get_char(&character_grid, x, y) == '@';
+    for (int y = 0; y < character_grid->height; y++) {
+        for (int x = 0; x < character_grid->width; x++) {
+            bool is_roll = character_grid_get_char(character_grid, x, y) == '@';
             if (!is_roll) continue;
 
-            char n = character_grid_get_char(&character_grid, x, y,  0, +1);
-            char e = character_grid_get_char(&character_grid, x, y, +1,  0);
-            char s = character_grid_get_char(&character_grid, x, y,  0, -1);
-            char w = character_grid_get_char(&character_grid, x, y, -1,  0);
+            char n = character_grid_get_char(character_grid, x, y, 0, +1);
+            char e = character_grid_get_char(character_grid, x, y, +1, 0);
+            char s = character_grid_get_char(character_grid, x, y, 0, -1);
+            char w = character_grid_get_char(character_grid, x, y, -1, 0);
 
-            char ne = character_grid_get_char(&character_grid, x, y, +1, +1);
-            char se = character_grid_get_char(&character_grid, x, y, +1, -1);
-            char sw = character_grid_get_char(&character_grid, x, y, -1, -1);
-            char nw = character_grid_get_char(&character_grid, x, y, -1, +1);
+            char ne = character_grid_get_char(character_grid, x, y, +1, +1);
+            char se = character_grid_get_char(character_grid, x, y, +1, -1);
+            char sw = character_grid_get_char(character_grid, x, y, -1, -1);
+            char nw = character_grid_get_char(character_grid, x, y, -1, +1);
 
             int adjacent_roll_count = 0;
 
@@ -104,8 +110,72 @@ u64 day_4_part_1(FILE *file) {
     return result;
 }
 
+static void remove_forkliftable_rolls(Character_Grid *src_character_grid,
+                                      Character_Grid *dest_character_grid) {
+    for (int y = 0; y < src_character_grid->height; y++) {
+        for (int x = 0; x < src_character_grid->width; x++) {
+            bool is_roll = character_grid_get_char(src_character_grid, x, y) == '@';
+            if (!is_roll) continue;
+
+            char n = character_grid_get_char(src_character_grid, x, y, 0, +1);
+            char e = character_grid_get_char(src_character_grid, x, y, +1, 0);
+            char s = character_grid_get_char(src_character_grid, x, y, 0, -1);
+            char w = character_grid_get_char(src_character_grid, x, y, -1, 0);
+
+            char ne = character_grid_get_char(src_character_grid, x, y, +1, +1);
+            char se = character_grid_get_char(src_character_grid, x, y, +1, -1);
+            char sw = character_grid_get_char(src_character_grid, x, y, -1, -1);
+            char nw = character_grid_get_char(src_character_grid, x, y, -1, +1);
+
+            int adjacent_roll_count = 0;
+
+            if (n == '@') adjacent_roll_count++;
+            if (e == '@') adjacent_roll_count++;
+            if (s == '@') adjacent_roll_count++;
+            if (w == '@') adjacent_roll_count++;
+
+            if (ne == '@') adjacent_roll_count++;
+            if (se == '@') adjacent_roll_count++;
+            if (sw == '@') adjacent_roll_count++;
+            if (nw == '@') adjacent_roll_count++;
+
+            if (adjacent_roll_count < 4) {
+                character_grid_set_char(dest_character_grid, x, y, '.');
+            }
+        }
+    }
+}
+
+u64 day_4_part_1(FILE *file) {
+    Character_Grid character_grid;
+    character_grid_init(&character_grid, file);
+
+    u64 result = count_forkliftable_rolls(&character_grid);
+
+    return result;
+}
+
 u64 day_4_part_2(FILE *file) {
     u64 result = 0;
+
+    Character_Grid character_grid;
+    character_grid_init(&character_grid, file);
+    
+    Character_Grid tmp_grid;
+    character_grid_init(&tmp_grid, file);
+
+    for (;;) {
+        memcpy(tmp_grid.data, character_grid.data, sizeof(s8)*character_grid.width*character_grid.height);
+
+        int forkliftable_count = count_forkliftable_rolls(&character_grid);
+        if (forkliftable_count <= 0) {
+            break;
+        }
+
+        remove_forkliftable_rolls(&tmp_grid, &character_grid);
+
+        result += forkliftable_count;
+    }
 
     return result;
 }
